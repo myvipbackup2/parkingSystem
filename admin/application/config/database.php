9d6703034a61fca6da18244faca1d1c48f4a43d6
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-04-17 15:31:24 +0800

    更新logo,数据库连接

--- admin/application/config/database.php
+++ admin/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.0.0.1',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'yueju',
+	'database' => 'parking_system',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
