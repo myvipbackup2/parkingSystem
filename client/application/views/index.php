80fbd72d9c74627f3fc6544ac985b318ba9c629d
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-04-18 00:43:11 +0800

    更新主页和数据库文件

--- client/application/views/index.php
+++ client/application/views/index.php
@@ -9,6 +9,7 @@
     <script src="js/rem.js"></script>
     <link rel="stylesheet" href="css/reset.css"/>
     <link rel="stylesheet" href="css/index.css"/>
+    <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=9w3UCQ9fjQApIUUGxwUu2YDArBsGanOG"></script>
 </head>
 <body>
 <div id="header">
@@ -28,7 +29,7 @@
             </li>
             <li class="menu-user">
                 <i class="iconfont icon-about"></i>
-                <a href="user"  data-stat-label="我的随心停">
+                <a href="user" data-stat-label="我的随心停">
                     我的随心停
                     <?php
                     $login_user = $this->session->userdata('userinfo');
@@ -50,15 +51,16 @@
     </div>
 </div>
 <div class="container">
-    <div id="nav">
-        <a href="#">
-            <img src="images/index-banner.jpg" alt=""/>
-        </a>
-    </div>
+    <!--    <div id="nav">-->
+    <!--        <a href="#">-->
+    <!--            <img src="images/index-banner.jpg" alt=""/>-->
+    <!--        </a>-->
+    <!--    </div>-->
+    <div id="allmap" style="height: 230px;"></div>
     <div class="search">
         <div class="fl search-box">
             <form action="park/index_search" method="post">
-                <input class="fl search-box-ipt" placeholder="搜索停车场..." type="text" name="content" />
+                <input class="fl search-box-ipt" placeholder="搜索停车场..." type="text" name="content"/>
                 <input class="fl search-box-btn" type="submit" value=""/>
             </form>
 
@@ -72,19 +74,19 @@
         <div class="c-item-box">
             <?php
             foreach ($result as $index => $park) {
-            ?>
-            <div class="c-item">
-                <a href="park/detail/<?php echo $park->park_id ?>">
-                    <div class="p-img">
-                        <img src="<?php echo ADMINPATH.$park->imgs[0]->img_thumb_src; ?>" alt=""/>
-                    </div>
-                    <div class="p-info">
-                        <div class="fl p-title"><?php echo $park->title ?></div>
-                        <div class="fr p-address"><?php echo $park->region ?></div>
-                        <div class="p-detail"><?php echo $park->description ?></div>
-                    </div>
-                </a>
-            </div>
+                ?>
+                <div class="c-item">
+                    <a href="park/detail/<?php echo $park->park_id ?>">
+                        <div class="p-img">
+                            <img src="<?php echo ADMINPATH . $park->imgs[0]->img_thumb_src; ?>" alt=""/>
+                        </div>
+                        <div class="p-info">
+                            <div class="fl p-title"><?php echo $park->title ?></div>
+                            <div class="fr p-address"><?php echo $park->region ?></div>
+                            <div class="p-detail"><?php echo $park->description ?></div>
+                        </div>
+                    </a>
+                </div>
                 <?php
             }
             ?>
@@ -123,5 +125,34 @@
 </div>
 <script src="js/zepto.js"></script>
 <script src="js/header.js"></script>
+<script type="text/javascript">
+    // 百度地图API功能，根据浏览器自动定位
+    var map = new BMap.Map("allmap");
+    var point = new BMap.Point(126.65771686,45.77322463);//默认黑大位置
+    map.centerAndZoom(point, 18);
+
+    var geolocation = new BMap.Geolocation();
+    geolocation.getCurrentPosition(function (r) {
+        if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+            var mk = new BMap.Marker(r.point);
+            map.addOverlay(mk);
+            map.panTo(r.point);
+//            alert('您的位置：' + r.point.lng + ',' + r.point.lat);
+        }
+        else {
+//            alert('failed' + this.getStatus());
+        }
+    }, {enableHighAccuracy: true})
+    //关于状态码
+    //BMAP_STATUS_SUCCESS	检索成功。对应数值“0”。
+    //BMAP_STATUS_CITY_LIST	城市列表。对应数值“1”。
+    //BMAP_STATUS_UNKNOWN_LOCATION	位置结果未知。对应数值“2”。
+    //BMAP_STATUS_UNKNOWN_ROUTE	导航结果未知。对应数值“3”。
+    //BMAP_STATUS_INVALID_KEY	非法密钥。对应数值“4”。
+    //BMAP_STATUS_INVALID_REQUEST	非法请求。对应数值“5”。
+    //BMAP_STATUS_PERMISSION_DENIED	没有权限。对应数值“6”。(自 1.1 新增)
+    //BMAP_STATUS_SERVICE_UNAVAILABLE	服务不可用。对应数值“7”。(自 1.1 新增)
+    //BMAP_STATUS_TIMEOUT	超时。对应数值“8”。(自 1.1 新增)
+</script>
 </body>
 </html>
