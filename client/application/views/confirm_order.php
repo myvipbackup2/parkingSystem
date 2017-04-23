18f62f72225bd53b2e851c5d21d365d879b199af
Lcj-space <1367546524@qq.com>
2017-04-23 22:46:15 +0800

    给当前页面添加一个了倒计时的功能

--- client/application/views/confirm_order.php
+++ client/application/views/confirm_order.php
@@ -97,7 +97,14 @@
             <span class="info-content fr"><?php echo $endTime; ?>时离开</span>
         </div>
         <div class="line clearfix">
-            <span class="info-content fr">共<?php echo $days; ?>小时</span>
+            <span class="info-title fl">剩余停车时间</span>
+            <span class="info-content fr" id="t_s">00秒</span>
+            <span class="info-content fr" id="t_h">00时</span>
+            <span class="info-content fr" id="t_m">00分</span>
+            <span class="info-content fr" id="t_d">00天</span>
+        </div>
+        <div class="line clearfix">
+            <span class="info-content fr"></span>
         </div>
     </div>
 
@@ -122,6 +129,50 @@
         });
 
     });
+</script>
+<script>
+    function formatTen(num) {
+        return num > 9 ? (num + "") : ("0" + num);
+    }
+
+    function GetRTime(){
+//        var EndTime = new Date(parseInt((<?php //echo $endTime; ?>//).replace(/-/g, "/"))); //截止时间 前端路上 http://www.51xuediannao.com/qd63/
+//        alert(EndTime);
+
+        var orderDate = new Date('<?php echo $endTime.':00:00'; ?>'.replace(/-/g, "/")); //字符串转成时间戳
+        var oDate = new Date();
+//        var gap = Math.abs(d1-d2)/1000/60/60;
+//        oDate = new Date(orderDate-oDate);
+        var y = oDate.getFullYear();
+        var M = oDate.getMonth() + 1;
+        var p = oDate.getDate();
+        var x = oDate.getHours();
+        var minute = oDate.getMinutes();
+        var second = oDate.getSeconds();
+        var e = y + '-' + formatTen(M) + '-' + formatTen(p) + ' ' + formatTen(x) + ':' +formatTen(minute) + ':' + formatTen(second);
+        var NowTime  = (e.replace(/-/g, "/"));
+        NowTime = new Date(NowTime);
+//        var gap = Math.abs(orderDate-NowTime)/1000;
+        console.log(NowTime,orderDate);
+//        console.log(gap);
+//        var NowTime = new Date();
+//        console.log(e);
+        var t =orderDate.getTime() - NowTime.getTime();
+        var d=Math.floor(t/1000/60/60/24);
+        var h=Math.floor(t/1000/60/60%24);
+        var m=Math.floor(t/1000/60%60);
+        var s=Math.floor(t/1000%60);
+
+        document.getElementById("t_d").innerHTML = d + "天";
+        document.getElementById("t_h").innerHTML = h + "时";
+        document.getElementById("t_m").innerHTML = m + "分";
+        document.getElementById("t_s").innerHTML = s + "秒";
+        if(NowTime==orderDate){
+            clearInterval(timer);
+            $.get()
+        }
+    }
+    timer=setInterval(GetRTime,1000);
 </script>
 </body>
 </html>
