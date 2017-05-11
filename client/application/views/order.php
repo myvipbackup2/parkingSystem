62c40ea917674a3e70c34211232f1f96473aabfb
myvipbackup2 <lzxvipmail@vip.qq.com>
2017-05-12 01:00:07 +0800

    订单倒序排列，修改css样式

--- client/application/views/order.php
+++ client/application/views/order.php
@@ -30,7 +30,7 @@
             </li>
             <li class="menu-user">
                 <i class="iconfont icon-about"></i>
-                <a href="user"  data-stat-label="我的随心停">
+                <a href="user" data-stat-label="我的随心停">
                     我的随心停
                     <?php
                     $login_user = $this->session->userdata('userinfo');
@@ -54,7 +54,8 @@
 <div id="order-container">
     <div class="tab-bar">
         <a href="javascript:;" :class="{'item':show,'current':showSpan}" @click="getparks('un_order')"><span>未完成</span></a>
-        <a href="javascript:;" :class="{'item':show,'current':!showSpan}"  @click="getparks('order')"><span>已完成</span></a>
+        <a href="javascript:;" :class="{'item':show,'current':!showSpan}"
+           @click="getparks('order')"><span>已完成</span></a>
     </div>
     <div class="pro-container">
         <div class="pro-item" v-for='(order,index) in orderList'>
@@ -64,21 +65,22 @@
                         <img :src="'<?php echo ADMINPATH ?>'+order.imgs[0].img_thumb_src" alt=""/>
                     </a>
                 </div>
-                <div class="fr p-info">
-<!--<!--                    <h2 class="p-title">{{order.park.title}}</h2>-->
-<!--<!--                    <div class="p-style p-address">地址：{{order.park.city}}{{order.park.region}}{{order.park.street}}</div>-->
-<!--<!--                    <div class="p-style p-type">-->
-<!---->
-<!--                    </div>-->
-                    <div class="p-btns-box" v-show="showSpan">
-<!--                        <a class="p-btns p-cancel" href="#">取消订单</a>-->
-                        <a href="javascript:;" class="p-btns p-cancel cancel_order" @click="delOrder(order.order_id,index)" v-show="!isPay(order.status)&&!isRefund(order.status)">取消订单</a>
-                        <a class="p-btns p-update" :href="'welcome/confirmorder?order_id='+order.order_id" v-show="!isPay(order.status)&&!isRefund(order.status)">去支付</a>
-                        <a class="p-btns p-update" v-show="isPay(order.status)&&!isRefund(order.status)" :href="'order/apply_refund?order_no='+order.order_no">申请退款</a>
-                        <a class="p-btns p-update" v-show="isRefund(order.status)" style="background: #eee;color: #ccc;">正在进行</a>
+                <div class="fr p-info" style="text-align: center;">
+                    <div class="p-btns-box" v-show="showSpan" style="text-align: center;">
+                        <a href="javascript:;" class="p-btns p-cancel cancel_order"
+                           @click="delOrder(order.order_id,index)"
+                           v-show="!isPay(order.status)&&!isRefund(order.status)">取消订单</a>
+                        <a class="p-btns p-update" :href="'welcome/confirmorder?order_id='+order.order_id"
+                           v-show="!isPay(order.status)&&!isRefund(order.status)">去支付</a>
+                        <a class="p-btns p-update" v-show="isPay(order.status)&&!isRefund(order.status)"
+                           :href="'order/apply_refund?order_no='+order.order_no">申请退款</a>
+                        <a class="p-btns p-update" v-show="isRefund(order.status)"
+                           style="background: #eee;color: #ccc;">正在进行</a>
                     </div>
                     <div class="p-btns-box" v-show="!showSpan">
-                        <a class="p-btns p-cancel" :href="'welcome/add_comment?orderId='+order.order_id+'&parkId='+order.park_id" v-show="order.comment">评论</a>
+                        <a class="p-btns p-cancel"
+                           :href="'welcome/add_comment?orderId='+order.order_id+'&parkId='+order.park_id"
+                           v-show="order.comment">评论</a>
                         <a class="p-btns p-cancel" href="javascript:;" v-show="!order.comment">已评论</a>
                         <a class="p-btns p-update" :href="'order/order_detail?orderId='+order.order_id">查看详情</a>
                     </div>
@@ -88,8 +90,8 @@
                 停车时间：{{order.start_time}} -离开时间：{{order.end_time}}
             </div>
         </div>
-       <!-- <div v-for='(park,index) in orderList' :class="{pic_group2:index%3==2,pic_group:index%3!=2}" >
-            <div class="pic_imgClass"><a class="pic_imgClass" :href="'park/detail/' + park.park_id" > <img :src="'<?php /*echo ADMINPATH */?>'+park.img_thumb_src" alt=""></a></div>
+        <!-- <div v-for='(park,index) in orderList' :class="{pic_group2:index%3==2,pic_group:index%3!=2}" >
+            <div class="pic_imgClass"><a class="pic_imgClass" :href="'park/detail/' + park.park_id" > <img :src="'<?php /*echo ADMINPATH */ ?>'+park.img_thumb_src" alt=""></a></div>
             <ul class="pic_dis">
                 <li ><span class="pic_name">{{park.title}}</span><span class="pic_name" style="float:right; color: #f00; margin-right: 10px;">￥{{park.price}}</span></li>
                 <li><span class="pic_name" style="font-size: 12px;">{{park.street}}</span></li>
@@ -98,7 +100,23 @@
     </div>
     <!--confirm弹框-->
     <div v-show="isShow">
-        <div class="mint-msgbox-wrapper" style="position: absolute; z-index: 1011;"><div class="mint-msgbox"><div class="mint-msgbox-header"><div class="mint-msgbox-title">提示</div></div> <div class="mint-msgbox-content"><div class="mint-msgbox-message">确定取消订单?</div> <div class="mint-msgbox-input" style="display: none;"><input placeholder="" type="text"> <div class="mint-msgbox-errormsg" style="visibility: hidden;"></div></div></div> <div class="mint-msgbox-btns"><button class="mint-msgbox-btn mint-msgbox-cancel " @click="isConfirm(false)">取消</button> <button class="mint-msgbox-btn mint-msgbox-confirm " @click="isConfirm(true)">确定</button></div></div></div>
+        <div class="mint-msgbox-wrapper" style="position: absolute; z-index: 1011;">
+            <div class="mint-msgbox">
+                <div class="mint-msgbox-header">
+                    <div class="mint-msgbox-title">提示</div>
+                </div>
+                <div class="mint-msgbox-content">
+                    <div class="mint-msgbox-message">确定取消订单?</div>
+                    <div class="mint-msgbox-input" style="display: none;"><input placeholder="" type="text">
+                        <div class="mint-msgbox-errormsg" style="visibility: hidden;"></div>
+                    </div>
+                </div>
+                <div class="mint-msgbox-btns">
+                    <button class="mint-msgbox-btn mint-msgbox-cancel " @click="isConfirm(false)">取消</button>
+                    <button class="mint-msgbox-btn mint-msgbox-confirm " @click="isConfirm(true)">确定</button>
+                </div>
+            </div>
+        </div>
         <div class="v-modal" style="z-index: 1010;"></div>
     </div>
 </div>
@@ -112,14 +130,13 @@
     var vm = new Vue({
         el: '#order-container',
         data: {
-            show:true,
-            showSpan:true,
-            orderList:[],
-            isShow:false,
-            orderId:'',
-            index:'',
+            show: true,
+            showSpan: true,
+            orderList: [],
+            isShow: false,
+            orderId: '',
+            index: ''
         },
-
         methods: {
             loadData: function (isHot) {
                 var _this = this;
@@ -130,51 +147,52 @@
                 }).then(function (res) {
                     var result = res.data;
                     _this.orderList = result.data;
+                    _this.orderList = _this.orderList.reverse();//订单倒序
                 });
             },
             getparks: function (isHot) {
-                if(isHot == 'order'){    //un_order    order
+                if (isHot == 'order') {    //un_order    order
                     this.showSpan = false;
-                }else {
+                } else {
                     this.showSpan = true;
                 }
                 this.loadData(isHot);
             },
-            delOrder: function(orderId,index){
+            delOrder: function (orderId, index) {
                 this.orderId = orderId;
                 this.index = index;
                 this.isShow = true;
             },
-            isConfirm:function(isGo){
+            isConfirm: function (isGo) {
                 var _this = this;
-                if(isGo){
+                if (isGo) {
                     axios.get('Welcome/delete_order', {
                         params: {
                             orderId: _this.orderId
                         }
                     }).then(function (res) {
                         var result = res.data;
-                        if(result == 'success'){
-                            _this.orderList.splice(_this.index,1);
+                        if (result == 'success') {
+                            _this.orderList.splice(_this.index, 1);
                             _this.isShow = false;
-                        }else{
+                        } else {
                         }
                     });
-                }else{
+                } else {
                     _this.isShow = false;
                 }
             },
-            isPay:function (order_status) {
-                if(order_status=='已支付'){
+            isPay: function (order_status) {
+                if (order_status == '已支付') {
                     return true;
-                }else{
+                } else {
                     return false;
                 }
             },
-            isRefund:function (order_status) {
-                if(order_status=='申请退款'){
+            isRefund: function (order_status) {
+                if (order_status == '申请退款') {
                     return true;
-                }else{
+                } else {
                     return false;
                 }
             }
